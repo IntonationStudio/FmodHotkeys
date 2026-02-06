c2de4380b811f70bfc59932532b46aa9aa4fb7df
Edward Dai <edward.y.dai@gmail.com>
2026-02-06 02:00:58 -0800

    new hotkeys added

--- FmodHotkeys.js
+++ FmodHotkeys.js
@@ -6,9 +6,9 @@
 
    //Function 1: Add Loop Region to all currently selected Event
 studio.menu.addMenuItem({
-    name: "Add Loop Region to selection",
+    name: "Add Loop Region to selections",
     keySequence: "Shift+L",
-    isEnabled: function() { return studio.window.editorSelection().length; },
+    isEnabled: function() { return studio.window.editorSelection().length && studio.window.browserCurrent().length; },
     execute: function() {
         // Retrieve the current selected event
         var event = studio.window.browserCurrent();
@@ -18,8 +18,9 @@ studio.menu.addMenuItem({
         var track = studio.project.create("MarkerTrack");
         track.event = event;
 
-        selections.forEach(function(element) {
+        selections.forEach(function(element, index) {
             var loopRegion = studio.project.create("LoopRegion");
+            loopRegion.name = "Loop Region " + (index + 1);
             loopRegion.position = element.start;
             loopRegion.length = element.length;
             loopRegion.selector = event;
@@ -31,9 +32,9 @@ studio.menu.addMenuItem({
 
 //Function 2: Add Magnet Region to all currently selected Event
 studio.menu.addMenuItem({
-    name: "Add Magnet Region to selection",
+    name: "Add Magnet Region to selections",
     keySequence: "Shift+M",
-    isEnabled: function() { return studio.window.editorSelection().length; },
+    isEnabled: function() { return studio.window.editorSelection().length && studio.window.browserCurrent().length; },
     execute: function() {
         // Retrieve the current selected event
         var event = studio.window.browserCurrent();
@@ -43,12 +44,12 @@ studio.menu.addMenuItem({
         var track = studio.project.create("MarkerTrack");
         track.event = event;
 
-        selections.forEach(function(element) {
+        selections.forEach(function(element, index) {
             var loopRegion = studio.project.create("LoopRegion");
+            loopRegion.name = "Magnet Region " + (index + 1);
             loopRegion.position = element.start;
             loopRegion.length = element.length;
             loopRegion.looping = 2; // Magnet Region
-            loopRegion.selector = event;
             loopRegion.timeline = timeLine;
             loopRegion.markerTrack = track;
         });
@@ -56,7 +57,7 @@ studio.menu.addMenuItem({
 });
 
 
-//Function 2: Add new event with a timeline sheet
+//Function 3: Add new event with a timeline sheet
 studio.menu.addMenuItem({
     name: "Add new event with a timeline sheet",
     keySequence: "Ctrl+Alt+T",
@@ -64,5 +65,83 @@ studio.menu.addMenuItem({
         var event = studio.project.create("Event");
         event.name = "New Timeline Event";
         event.addGroupTrack("Audio Track");
+    }
+});
+
+//Function 4: Add Transition Region to selections
+studio.menu.addMenuItem({
+    name: "Add Transition Region to selections",
+    keySequence: "Shift+T",
+    isEnabled: function() { return studio.window.editorSelection().length && studio.window.browserCurrent().length; },
+    execute: function() {
+        // Retrieve the current selected event
+        var event = studio.window.browserCurrent();
+        var selections = studio.window.editorSelection();
+        var timeLine = event.timeline;
+        
+        var track = studio.project.create("MarkerTrack");
+        track.event = event;
+
+        selections.forEach(function(element, index) {
+            var TransitionRegion = studio.project.create("TransitionRegion");
+            TransitionRegion.name = "Transition Region " + (index + 1);
+            TransitionRegion.position = element.start;
+            TransitionRegion.length = element.length;
+            TransitionRegion.selector = event;
+            TransitionRegion.timeline = timeLine;
+            TransitionRegion.markerTrack = track;
+        });
+    }
+});
+
+//Function 5: Add Destination Region to selections
+studio.menu.addMenuItem({
+    name: "Add Destination Region to selections",
+    keySequence: "Shift+D",
+    isEnabled: function() { return studio.window.editorSelection().length && studio.window.browserCurrent().length; },
+    execute: function() {
+        // Retrieve the current selected event
+        var event = studio.window.browserCurrent();
+        var selections = studio.window.editorSelection();
+        var timeLine = event.timeline;
+        
+        var track = studio.project.create("MarkerTrack");
+        track.event = event;
+
+        selections.forEach(function(element, index) {
+            var DestinationRegion = studio.project.create("LoopRegion");
+            DestinationRegion.name = "Destination Region " + (index + 1);
+            DestinationRegion.looping = 0; // non looping aka destination region
+            DestinationRegion.position = element.start;
+            DestinationRegion.length = element.length;
+            DestinationRegion.selector = event;
+            DestinationRegion.timeline = timeLine;
+            DestinationRegion.markerTrack = track;
+        });
+    }
+});
+
+//Function 6: Add Destination Marker to selections
+studio.menu.addMenuItem({
+    name: "Add Destination Marker to start of selections",
+    keySequence: "Ctrl+Shift+D",
+    isEnabled: function() { return studio.window.editorSelection().length && studio.window.browserCurrent().length; },
+    execute: function() {
+        // Retrieve the current selected event
+        var event = studio.window.browserCurrent();
+        var selections = studio.window.editorSelection();
+        var timeLine = event.timeline;
+        
+        var track = studio.project.create("MarkerTrack");
+        track.event = event;
+
+        selections.forEach(function(element, index) {
+            var DestinationMarker = studio.project.create("NamedMarker");
+            DestinationMarker.name = "Destination Marker " + (index + 1);
+            DestinationMarker.position = element.start;
+            DestinationMarker.selector = event;
+            DestinationMarker.timeline = timeLine;
+            DestinationMarker.markerTrack = track;
+        });
     }
 });
